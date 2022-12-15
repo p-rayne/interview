fc40da4e972dd222f1d1c5c1fc637d86c68bd956
p-rayne <marlinaqua@gmail.com>
2022-12-15 16:25:40 +0300

    added html pages, views and urls

--- backend_interview/junior_question/urls.py
+++ backend_interview/junior_question/urls.py
@@ -1,3 +1,7 @@
 from django.urls import path
+from .views import QuestionListView, IndexTemplateView
 
-urlpatterns = []
+urlpatterns = [
+    path("", IndexTemplateView.as_view(), name="main"),
+    path("question/", QuestionListView.as_view(), name="question_list"),
+]
--- backend_interview/junior_question/views.py
+++ backend_interview/junior_question/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from django.views.generic import ListView, TemplateView
+from typing import Any, Dict
+from .models import Question
 
-# Create your views here.
+
+class QuestionListView(ListView):
+    model = Question
+    template_name = "junior_question/question.html"
+    queryset = (
+        Question.objects.all()
+        .select_related("category", "answer")
+        .order_by("category__id")
+    )
+
+
+class IndexTemplateView(TemplateView):
+    template_name = "index.html"
+
+    def get_context_data(self, **kwargs: Any) -> Dict[str, Any]:
+        context = super().get_context_data(**kwargs)
+        context["amount"] = Question.objects.count()
+        return context
