2f81b3567785429a525276082b239f1bd4808b55
p-rayne <marlinaqua@gmail.com>
2022-12-15 06:39:10 +0300

    Initial commit

---
+++ backend_interview/junior_question/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class JuniorQuestionConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'junior_question'
---
+++ backend_interview/junior_question/models.py
@@ -0,0 +1,26 @@
+from django.db import models
+
+
+class Question(models.Model):
+    question = models.TextField(verbose_name="Вопрос")
+    category = models.ForeignKey("Category", on_delete=models.SET_NULL, null=True)
+
+    def __str__(self) -> str:
+        return f"Вопрос №{self.pk}"
+
+
+class Answer(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    answer = models.TextField(verbose_name="Ответ")
+    code = models.TextField(null=True, verbose_name="Пример кода")
+    url = models.URLField()
+
+    def __str__(self) -> str:
+        return f"Ответ на вопрос №{self.question.id}"
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=60, verbose_name="Категория")
+
+    def __str__(self) -> str:
+        return self.name
