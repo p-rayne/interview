b8cf0413e5c954f2e297747f61168ba847b3f110
p-rayne <marlinaqua@gmail.com>
2022-12-15 08:17:24 +0300

    registered models in admin panel

--- backend_interview/junior_question/admin.py
+++ backend_interview/junior_question/admin.py
@@ -1,3 +1,33 @@
 from django.contrib import admin
+from .models import Question, Answer, Category
 
-# Register your models here.
+
+class QuestionAdmin(admin.ModelAdmin):
+    list_display = (
+        "id",
+        "question",
+        "category",
+    )
+    list_display_links = ("id", "question")
+    search_fields = ("id", "question")
+    list_filter = ("category",)
+
+
+class AnswerAdmin(admin.ModelAdmin):
+    list_display = ("id", "question_id", "answer")
+    list_display_links = ("id", "question_id")
+    search_fields = ("id", "question_id")
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = (
+        "id",
+        "name",
+    )
+    list_display_links = ("id", "name")
+    search_fields = ("id", "name")
+
+
+admin.site.register(Question, QuestionAdmin)
+admin.site.register(Answer, AnswerAdmin)
+admin.site.register(Category, CategoryAdmin)
